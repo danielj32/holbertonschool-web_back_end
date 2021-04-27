ff54953a20cba23ec3e7d279bf02af6a20178bb2
danielj32 <danieljuanito21@hotmail.com>
2021-04-26 21:50:37 -0500

    add duck-typed annotations

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""  first element of a sequence """
+
+from typing import Any, Sequence, Union
+
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """ firs element """
+    if lst:
+        return lst[0]
+    else:
+        return None
