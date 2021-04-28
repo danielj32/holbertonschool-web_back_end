e27b9c737a35e6662ae60b7c8d11d1ffe23f190e
danielj32 <danieljuanito21@hotmail.com>
2021-04-28 18:57:38 -0500

    fix codestyle

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,4 +9,4 @@ async def async_generator() -> Generator[float, None, None]:
     """ return the 10 random numbers. """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0,10)
+        yield random.uniform(0, 10)
