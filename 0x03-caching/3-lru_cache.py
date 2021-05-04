4c4552cf97200bba7e4e45414f5c3dedc99e913b
danielj32 <danieljuanito21@hotmail.com>
2021-05-03 22:13:01 -0500

    add new LRU

---
+++ 0x03-caching/3-lru_cache.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" LRU """
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ Class LRUCache  """
+
+    def __init__(self):
+        """ Init """
+        super().__init__()
+        self.ordered_cache_keys = []
+
+    def put(self, key, item):
+        """ Adds an  new item in the cache """
+        if not (key is None or item is None):
+            if (
+                len(self.cache_data.keys()) == BaseCaching.MAX_ITEMS and
+                (key not in self.cache_data.keys())
+            ):
+                lru = self.ordered_cache_keys[0]
+                print('DISCARD: {}'.format(lru))
+                self.ordered_cache_keys.pop(0)
+                del self.cache_data[lru]
+                self.cache_data[key] = item
+                self.ordered_cache_keys.append(key)
+            elif (
+                len(self.cache_data.keys()) == BaseCaching.MAX_ITEMS and
+                (key in self.cache_data.keys())
+            ):
+                self.ordered_cache_keys.remove(key)
+                self.ordered_cache_keys.append(key)
+                self.cache_data[key] = item
+            else:
+                self.ordered_cache_keys.append(key)
+                self.cache_data[key] = item
+
+    def get(self, key):
+        """ Gets an  new item by key """
+        if key not in self.cache_data.keys():
+            return None
+        else:
+            self.ordered_cache_keys.remove(key)
+            self.ordered_cache_keys.append(key)
+            return self.cache_data[key]
