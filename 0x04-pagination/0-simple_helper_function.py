f6a9c43c190c75a0ac67f3de41f0e6b187f84f73
danielj32 <danieljuanito21@hotmail.com>
2021-05-04 23:19:57 -0500

    add function 0. Simple helper function

---
+++ 0x04-pagination/0-simple_helper_function.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Simple helper function """
+
+
+def index_range(page, page_size):
+    """
+    Return a tuple of size two
+    containing a start index and an end
+    index corresponding to
+    the range of indexes to return in a list for
+    those particular pagination parameters
+    Page numbers are 1-indexed, i.e. the first page is page 1.
+    """
+    before = (page - 1) * page_size
+    return (before, before + page_size)
