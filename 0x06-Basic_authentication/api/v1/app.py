3d2220e1e18dcb6b75c3913c424454c4336c7fab
danielj32 <danieljuanito21@hotmail.com>
2021-05-12 23:35:14 -0500

    add Error handler: Unauthorized

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -12,6 +12,13 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+if os.getenv('AUTH_TYPE') == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+elif os.getenv('AUTH_TYPE') == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
@@ -19,6 +26,12 @@ def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def ifNot_authorized(error) -> str:
+    """ Unauthorized for handler """
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 if __name__ == "__main__":
