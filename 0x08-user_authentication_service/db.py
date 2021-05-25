b4a095a8fc9797269bba21d680cede9c96f6bbdc
danielj32 <danieljuanito21@hotmail.com>
2021-05-24 20:19:38 -0500

    fix path

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -1,16 +1,6 @@
-#!/usr/bin/env python3
-"""DB module
-"""
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
-
-from user import Base, User
-
 
 #!/usr/bin/env python3
-""" Module of class DB """
+""" Module of DB """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
