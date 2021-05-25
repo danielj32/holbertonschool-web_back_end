668df2f44300ca2e4746114881e10558a6c923d8
danielj32 <danieljuanito21@hotmail.com>
2021-05-24 20:17:34 -0500

    fix add

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -9,22 +9,28 @@ from sqlalchemy.orm.session import Session
 from user import Base, User
 
 
+#!/usr/bin/env python3
+""" Module of class DB """
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+from user import Base, User
+
+
 class DB:
-    """DB class
-    """
+    """ Definition of DB class """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        """ Initialize a new DB instance for the ORM """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self) -> Session:
-        """Memoized session object
-        """
+    def _session(self):
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
