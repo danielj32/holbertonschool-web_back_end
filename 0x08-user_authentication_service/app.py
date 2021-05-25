dc133f328c668c851a5334b955740de6489030f9
danielj32 <danieljuanito21@hotmail.com>
2021-05-24 23:10:43 -0500

    change name method

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
-def welcome():
+def home():
     """ return a JSON payload of the form """
     return jsonify({"message": "Bienvenue"})
 
