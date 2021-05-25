075df889e769348b9c6e657726a9934d60263d9f
danielj32 <danieljuanito21@hotmail.com>
2021-05-25 16:32:17 -0500

    new session

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -27,5 +27,23 @@ def users():
         return jsonify({"message": "email already registered"})
 
 
+@app.route('/sessions', methods=['POST'])
+def sessions():
+    """new session for the user,store it
+    the session ID as a cookie
+    with key "reference_id" on the response
+    and return a JSON payload """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    reference_id = AUTH.create_session(email)
+    if not reference_id:
+        abort(401)
+    response = make_response(jsonify({"email": email, "message": "logged in"}))
+    response.set_cookie("reference_id", reference_id)
+    return response
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
