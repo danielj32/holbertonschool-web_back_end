d82cf0fcdd6db02d2cd9a14966469104364dd30f
danielj32 <danieljuanito21@hotmail.com>
2021-05-25 18:13:01 -0500

    reset password

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -83,3 +83,14 @@ class Auth:
         """
         self._db.update_user(user_id, session_id=None)
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ Method that
+        reset a password """
+        try:
+            origin = self._db.find_user_by(email=email)
+            tokens = _generate_uuid()
+            self._db.update_user(origin.id, reset_token=tokens)
+            return tokens
+        except NoResultFound:
+            raise ValueError
