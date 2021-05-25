8426e07f1fbb31ca07ebb4033444210f7176f75e
danielj32 <danieljuanito21@hotmail.com>
2021-05-24 23:04:13 -0500

    fix 4

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
-"""6. Basic Flask app"""
+""" a basic Flask app. """
 from flask import Flask, jsonify, request, abort, make_response, redirect
 from auth import Auth
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
-@app('/', methods=['GET'])
-def home():
-    """  return a JSON
-    payload of the form """
+@app.route('/', methods=['GET'])
+def welcome():
+    """ returns a message when the route / is requested """
     return jsonify({"message": "Bienvenue"})
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
