28606bb270dc6bc79106d9d45acbcd2117459985
danielj32 <danieljuanito21@hotmail.com>
2021-05-24 22:35:39 -0500

    5. Register user

---
+++ 0x08-user_authentication_service/auth.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Auth db
+"""
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+import bcrypt
+import uuid
+
+
+def _hash_password(password: str) -> str:
+    """
+    salted hash of the input password,
+    hashed with bcrypt.hashpw
+    """
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """ Generate a new string
+    representation of a new UUID """
+    return str(uuid.uuid4())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ new user """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError('User {} already exists'.format(email))
+        except NoResultFound:
+            mypass = _hash_password(password)
+            user = self._db.add_user(email, mypass)
+            return user
