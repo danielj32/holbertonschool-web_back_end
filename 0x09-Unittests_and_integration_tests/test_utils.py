d4d2bb467e387b22763e0e69f84ac71f4c09a2eb
danielj32 <danieljuanito21@hotmail.com>
2021-06-01 17:32:29 -0500

    fix add

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -7,14 +7,14 @@ from unittest.mock import Mock, patch
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """ access_nested_map test class"""
+    """ test class"""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {'b': 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """ Method that tests access_nested_map function """
+        """ tests access_nested_map function """
         self.assertEquals(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
@@ -22,5 +22,5 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b"), KeyError)
     ])
     def test_access_nested_map_exception(self, nested_map, path, expected):
-        """ Method that tests access_nested_map's exception """
+        """ tests access_nested_map's exception """
         self.assertRaises(expected)
