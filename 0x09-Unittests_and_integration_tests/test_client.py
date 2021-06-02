3b4651cefc6768336a0231815147564a71cd20b4
danielj32 <danieljuanito21@hotmail.com>
2021-06-01 19:35:01 -0500

    testing for clients

---
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Tests for file clients """
+from client import GithubOrgClient
+from parameterized import parameterized
+from unittest.mock import patch
+from utils import get_json
+import unittest
+
+
+class GithubOrgClient(unittest.TestCase):
+    """ class test for For GitHub """
+    @parameterized.expand([
+        ("google"),
+        ("abc") 
+    ])
+    def test_org(self, org):
+        """ method testing """
+        testing = GithubOrgClient(org)
+        self.assertEqual(testing.org(), "https://api.github.com/orgs/{}".format(org))
