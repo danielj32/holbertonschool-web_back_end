93ea4192de7a1867e30fc2d7cca3ccd8614a1715
danielj32 <danieljuanito21@hotmail.com>
2021-06-03 18:23:14 -0500

    add task 2

---
+++ 0x0A-i18n/2-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" Flask app
+basic components """
+from flask import Flask, app, render_template, g, request
+from flask_babel import Babel
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """ class for
+    Basic Babel setup"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object('1-app.Config')
+
+
+@app.route('/')
+def helloWorld():
+    """return render
+    index template """
+    return render_template("1-index.html")
+
+
+@babel.localeselector
+def get_locale():
+    """ determine the
+    best match with
+    our supported languages """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
