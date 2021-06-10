b41c61bad1385d818db2aa84bfd56188cd168060
danielj32 <danieljuanito21@hotmail.com>
2021-06-10 12:23:55 -0500

    fix add

--- 0x0C-MySQL_Advanced/1-country_users.sql
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,8 @@
--- We are all unique!
--- creates a table users
+-- We are 
+-- all unique!
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
+    country ENUM('US','CO','TN') NOT NULL DEFAULT 'US'
 );
