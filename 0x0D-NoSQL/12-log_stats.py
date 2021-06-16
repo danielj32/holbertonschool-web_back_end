a39245a5adc3524221267d4929792a5190840d37
danielj32 <danieljuanito21@hotmail.com>
2021-06-15 20:10:26 -0700

    [fix]

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -7,19 +7,19 @@ from pymongo import MongoClient
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
-    print(f'{nginx_collection.find().count_docments()} logs')
-    print('Methods:')
-
+    x = len(list(nginx_collection.find()))
+    print(x, "logs\nMethods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-    for _method in methods:
-        method_count = nginx_collection.find(
-            {'method': _method}
-        ).count_documents()
-
-        print(f'\tmethod {_method}: {method_count}')
-
-    status_check_count = nginx_collection.find(
-        {'method': 'GET', 'path': '/status'}
-    ).count_documents()
-    print(f'{status_check_count} status check')
+    for m in methods:
+        print(
+            "\tmethod {}: {}".format(
+                m, len(list(nginx_collection.find({"method": m})))
+                )
+            )
+    print(
+        "{} status check".format(
+            len(list(
+                nginx_collection.find({"method": "GET", "path": "/status"})
+                ))
+            )
+        )
