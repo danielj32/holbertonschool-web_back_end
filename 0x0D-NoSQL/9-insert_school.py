8f1182e0e56fdcc943cb089e79d8475b60292db9
danielj32 <danieljuanito21@hotmail.com>
2021-06-15 19:43:09 -0700

    [add]

---
+++ 0x0D-NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" function that inserts a new document in a collection
+based on kwargs """
+
+
+def insert_school(mongo_collection, **kwargs):
+    """ inserts a new document in a collection
+    based on kwargs """
+    col = mongo_collection.insert_one(kwargs)
+    return col.inserted_id
