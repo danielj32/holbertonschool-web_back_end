d9b96f4fe82bbda7359439e2ba09eea2751a84fa
danielj32 <danieljuanito21@hotmail.com>
2021-06-24 17:45:50 -0500

    add task

--- 0x10-ES6_classes/4-pricing.js
+++ 0x10-ES6_classes/4-pricing.js
@@ -1,41 +1,41 @@
-import Currency from "./3-currency";
+import Currency from './3-currency';
 
 export default class Pricing {
-	constructor(amount, currency) {
-		if (typeof amount !== 'number' || !(currency instanceof Currency)) {
-			throw (TypeError('wrong parameter type'));
-		}
-		this.__amount = amount;
-		this.__currency = currency;
-		}
+  constructor(amount, currency) {
+    if (typeof amount !== 'number' || !(currency instanceof Currency)) {
+      throw (TypeError('wrong parameter type'));
+    }
+    this._amount = amount;
+    this._currency = currency;
+  }
 
-		get amount() {
-			return this.__amount;
-		}
+  get amount() {
+    return this._amount;
+  }
 
-		get currency() {
-			return this.__currency;
-		}
+  get currency() {
+    return this._currency;
+  }
 
-		set amount(amount) {
-			if (typeof amount !== 'number') {
-				throw (TypeError('wrong parameter type'));
-			}
-			this.__amount = amount;
-		}
+  set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw (TypeError('wrong parameter type'));
+    }
+    this._amount = amount;
+  }
 
-		set currency(currency) {
-			if (!(currency instanceof Currency)) {
-				throw (TypeError('wrong parameter type'));
-			}
-			this.__currency = currency;
-		}
+  set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw (TypeError('wrong parameter type'));
+    }
+    this._currency = currency;
+  }
 
-		displayFullPrice() {
-			return `${this.__amount} ${this.__currency.name} (${this.__currency.code})`;
-		}
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
 
-		static convertPrice(amount, conversionRate) {
-			return amount * conversionRate;
-		}
-	}
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
