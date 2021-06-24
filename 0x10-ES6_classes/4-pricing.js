f610c5636bd186cbb71098e808b6462565e6824d
danielj32 <danieljuanito21@hotmail.com>
2021-06-24 17:38:24 -0500

    add task

--- 0x10-ES6_classes/4-pricing.js
+++ 0x10-ES6_classes/4-pricing.js
@@ -8,33 +8,33 @@ export default class Pricing {
       this.__amount = amount;
       this.__currency = currency;
     }
-  
+
     get amount() {
       return this.__amount;
     }
-  
+
     get currency() {
       return this.__currency;
     }
-  
+
     set amount(amount) {
       if (typeof amount !== 'number') {
         throw (TypeError('wrong parameter type'));
       }
       this.__amount = amount;
     }
-  
+
     set currency(currency) {
       if (!(currency instanceof Currency)) {
         throw (TypeError('wrong parameter type'));
       }
       this.__currency = currency;
     }
-  
+
     displayFullPrice() {
       return `${this.__amount} ${this.__currency.name} (${this.__currency.code})`;
     }
-  
+
     static convertPrice(amount, conversionRate) {
       return amount * conversionRate;
     }
