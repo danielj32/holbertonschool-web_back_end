4865ab7c3f87de4924ca43ad79dbf0666db1676d
danielj32 <danieljuanito21@hotmail.com>
2021-06-29 16:21:16 -0500

    add task

--- 0x11-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x11-ES6_data_manipulation/3-get_ids_sum.js
@@ -2,6 +2,6 @@ export default function getStudentIdsSum(students) {
   if (Array.isArray(students) === false) {
     return 0;
   }
-  let get_sum = (sum, i) => sum + i.id;
-  return students.reduce(get_sum, 0);
+  let red = (sum, s) => sum + s.id;
+  return students.reduce(red, 0);
 }
