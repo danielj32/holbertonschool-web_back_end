7ca093d69fbdf360e356d5b21410a6172aeda503
danielj32 <danieljuanito21@hotmail.com>
2021-06-29 16:10:59 -0500

    add task

--- 0x11-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x11-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,9 @@
 export default function getStudentsByLocation(students, city) {
-  if (Array.isArray(students) === false) {
-    return [];
-  } else {
-    return students.filter((y) => y.location === city);
-  }
+	if (Array.isArray(students) === false) {
+		return [];
+	}
+	else
+	{
+		return students.filter((y) => y.location === city);
+	}
 }
